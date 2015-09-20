2b887f0d9d63a0bd18b762004d29a7264dcfc3f6
karlmontenegro <carlos.hurtadom@pucp.pe>
2015-09-20 15:32:44 -0500

    put all the files inside...

---
+++ gui/simple-route-manager-gui/src/simple/route/manager/gui/SimpleRouteManagerGui.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simple.route.manager.gui;
+
+import javax.swing.JFrame;
+
+public class SimpleRouteManagerGui {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFrame loginFrame = new loginWindow("Login - Simple Route Manager v.1.0.0");
+        loginFrame.setVisible(true);
+    }
+    
+}
