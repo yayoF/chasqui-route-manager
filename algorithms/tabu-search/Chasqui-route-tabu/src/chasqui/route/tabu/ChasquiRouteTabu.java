de5615e788352f6c949609f52cd4a7bf58adf5cf
Joca <jpereyraleon@gmail.com>
2015-09-30 20:31:47 -0500

    fix useless classes

--- algorithms/tabu-search/Chasqui-route-tabu/src/chasqui/route/tabu/ChasquiRouteTabu.java
+++ algorithms/tabu-search/Chasqui-route-tabu/src/chasqui/route/tabu/ChasquiRouteTabu.java
@@ -6,15 +6,11 @@
 package chasqui.route.tabu;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.concurrent.ThreadLocalRandom;
-
 /**
  *
  * @author Joca
